3997b9b0fbf0675699ccd18270a8f07884485878
Ariel Zhen <zhenpingping@live.com>
2018-05-15 07:16:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
   if num % 3 == 0
-    "Fizz"
+    return "Fizz"
   elsif num % 5 == 0
-    "Buzz"
+    return "Buzz"
   elsif num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
+    return "FizzBuzz"
   else
-    nil
+    return nil
 end
